164b85f76cbb46ed0954839c87bfc5091124484c
emkr-13 <krisna.rata13@gmail.com>
2024-04-29 17:15:37 +0700

    update model klasfikasi

--- page1.py
+++ page1.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import joblib
+
+def load_model(model_path):
+    loaded_model_data = joblib.load(model_path)
+    loaded_model = loaded_model_data['model']
+    loaded_vectorizer = loaded_model_data['vectorizer']
+    loaded_label_encoder = loaded_model_data['label_encoder']
+    return loaded_model, loaded_vectorizer, loaded_label_encoder
+
+def predict_sentiment(model, vectorizer, label_encoder, text_data):
+    new_data_features = vectorizer.transform(text_data)
+    predictions = model.predict(new_data_features)
+    predicted_sentiments = label_encoder.inverse_transform(predictions)
+    return predicted_sentiments
 
 def page1_content():
-    st.title('Page 1')
-    st.write('This is the content of page 1.')
+    st.title('Teks Klasifikasi')
+    st.write('Masukkan teks untuk melakukan klasifikasi sentimen:')
+    text_input = st.text_area('Input Teks:')
+    model_path = './resource/logistic_regression_with_vectorizer.pkl'
+    loaded_model, loaded_vectorizer, loaded_label_encoder = load_model(model_path)
+    
+    if st.button('Prediksi Sentimen'):
+        if text_input:
+            new_data = [text_input]
+            predicted_sentiments = predict_sentiment(loaded_model, loaded_vectorizer, loaded_label_encoder, new_data)
+            st.write(f'Prediksi Sentimen: {predicted_sentiments[0]}')
+
